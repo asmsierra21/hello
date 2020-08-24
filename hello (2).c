05a9e3682e3596b9feaec2e0904de6f44571e62b
asmsierra21 <69830582+asmsierra21@users.noreply.github.com>
2020-08-24 18:14:07 -0500

    Initial commit

---
+++ hello (2).c
@@ -0,0 +1,17 @@
+/**
+ * Author: Alexandra Sierra-Mendoza
+ *         asierra-mendoza2@huskers.unl.edu
+ * Date: 2020/08/24
+ *
+ * Working with GitHub
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Alexandra Sierra-Mendoza\n");
+
+  return 0;
+}
