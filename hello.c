8d7252eb445b3fa7b14cb8f0a9b5089d674cc718
Alexandra Sierra-Mendoza <asierra-mendoza2@huskers.unl.edu>
2021-08-26 14:56:27 +0000

    initail commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Alexandra Sierra-Mendoza
+ *         asierra-mendoza2@unl.edu
+ * Date: 2021/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Alexandra Sierra-Mendoza\n");
+
+  return 0;
+}
